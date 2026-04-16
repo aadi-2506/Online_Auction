84a614fc7d0683bac0647a77c38df22d0b253ce5
Aaditheya <aaditheyayarrarapu@gmail.com>
2026-04-16 11:55:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -164,4 +164,4 @@ if __name__ == "__main__":
     os.makedirs("static/uploads", exist_ok=True)
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
